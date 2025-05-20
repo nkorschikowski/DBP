b689a4af557987fc68360d09187403efe4302bb6
Nico <167690530+nkorschikowski@users.noreply.github.com>
2025-05-21 00:55:29 +0200

    deleted out-commented code and unused imports

--- Example/example.java
+++ Example/example.java
@@ -4,12 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 public class example {
 
@@ -52,33 +49,3 @@ public class example {
         con.close();
     }
 }
-
-// funktioniert wenn run java
-
-// public class example {
-
-// public static void main(String[] args) throws SQLException {
-// // String sql = "SelectFROM name";
-// String url = "jdbc:postgresql://localhost:5432/ExampleDB";
-// String name = "postgres";
-// String password = "#N9374h03";
-
-// try {
-// Class.forName("org.postgresql.Driver");
-// } catch (ClassNotFoundException e) {
-// e.printStackTrace();
-// }
-
-// Connection con = DriverManager.getConnection(url, name, password);
-// if (con != null) {
-// System.out.println("Connected");
-// } else {
-// System.out.println("Not connected!");
-// }
-// // Statement st = con.createStatement();
-// // ResultSet rs = st.executeQuery(sql);
-// // rs.next();
-// // String result = rs.getString(1);
-// // System.out.println(result);
-// }
-// }
