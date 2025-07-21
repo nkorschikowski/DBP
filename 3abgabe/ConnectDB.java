7b761706065a9924139cd6a858c3e1f512158506
merlinEgress <merlin.hofmann@mailbox.org>
2025-07-21 17:56:14 +0200

    connect to db via hibernate + create test table

---
+++ 3abgabe/ConnectDB.java
@@ -0,0 +1,70 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
+public class ConnectDB {
+
+    public Connection connect_to_db(String dbname,
+        String user,
+        String pass
+    ){
+    Connection conn = null;
+    try{
+        Class.forName("org.postgresql.Driver");
+        conn = DriverManager.getConnection("jdbc:postgresql://localhost:5435/"+dbname, user, user);
+        if(conn!=null){
+            System.err.println("Connected to DB...");
+        }else{
+            System.err.println("Connection failed");
+        }
+    }catch(Exception e){
+        System.err.println(e);
+    }
+    return conn;
+    }
+
+    public void createTable(Session session, 
+        String tableName
+    ){
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+
+            String query = "CREATE TABLE " + tableName + " ("
+                        + "id SERIAL PRIMARY KEY, "
+                        + "name VARCHAR(200), "
+                        + "parameter VARCHAR(200))";
+
+            session.createNativeQuery(query).executeUpdate();
+
+            transaction.commit();
+            System.out.println("Table " + tableName + " created...");
+        }catch(Exception e){
+            System.err.println(e);
+        }
+
+    }
+
+    public void deleteTable(Session session, 
+        String tableName
+    ){
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            String query = "drop table " + tableName + ";";
+            session.createNativeQuery(query).executeUpdate();
+            transaction.commit();
+            System.out.println("Table " + tableName + " deleted...");
+        }catch(Exception e){
+            System.err.println(e);
+        }
+
+    }
+
+    
+}
---
+++ 3abgabe/Main.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a SessionFactory using the XML configuration
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+        // Open a session
+        Session session = sessionFactory.openSession();
+        System.out.println("Connected to the database...");
+
+        ConnectDB db = new ConnectDB();
+        db.deleteTable(session, "test_coloumn");
+        
+
+
+        // Always close resources
+        session.close();
+        sessionFactory.close();
+    }
+}
