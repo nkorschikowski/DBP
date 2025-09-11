2feed6f3e1bb4b281e51d5a75d3c4b65733d08ba
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 16:51:55 +0200

    Update Kauf.java

--- src/main/java/com/uni/Kauf.java
+++ src/main/java/com/uni/Kauf.java
@@ -1,20 +1,66 @@
 package com.uni;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "kauf")
-public class Kauf{
+public class Kauf {
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int kauf_id;
-    private int filiale_id;
-    private int person_id;
-    //private date kaufdatum; //TODO: Date
-    //TODO: FK
 
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "filiale_id", referencedColumnName = "filiale_id", nullable = true)
+    private Filiale filiale;
 
-    public Kauf(){}
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "person_id", referencedColumnName = "person_id", nullable = true)
+    private Person person;
+
+    // TODO: Date kaufdatum
+
+    public int getKauf_id() {
+        return kauf_id;
+    }
+
+    public void setKauf_id(int kauf_id) {
+        this.kauf_id = kauf_id;
+    }
+
+    public Filiale getFiliale() {
+        return filiale;
+    }
+
+    public void setFiliale(Filiale filiale) {
+        this.filiale = filiale;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Kauf(int kauf_id, Filiale filiale, Person person) {
+        this.kauf_id = kauf_id;
+        this.filiale = filiale;
+        this.person = person;
+    }
+
+    public Kauf() {
+        // Default constructor for JPA
+    }
 }
