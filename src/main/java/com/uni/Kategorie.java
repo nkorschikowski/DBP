1d4e4308d13762ae88c840a6cef7a26f2d11421b
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 16:50:45 +0200

    Update Kategorie.java

--- src/main/java/com/uni/Kategorie.java
+++ src/main/java/com/uni/Kategorie.java
@@ -1,19 +1,45 @@
 package com.uni;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "kategorien")
 public class Kategorie{
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private int kategorie_id; //TODO: serial
-    private int oberkategorie_id; 
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+    private int kategorie_id;
+    @ManyToOne 
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name="oberkategorie_id", referencedColumnName="kategorie_id")
+    private Kategorie oberkategorie; 
     //TODO: self refferential
-    //TODO: FK
 
+    private int get_kategorie_id() {
+        return kategorie_id;
+    }
+    private void set_kategorie_id(int kategorie_id) {
+        this.kategorie_id = kategorie_id;
+    }
+    private Kategorie get_oberkategorie() {
+        return oberkategorie;
+    }
+    private void set_oberkategorie_id(Kategorie oberkategorie_id) {
+        this.oberkategorie = oberkategorie_id;
+    }
+    private Kategorie(int kategorie_id,
+     Kategorie oberkategorie
+      ){
+        this.kategorie_id = kategorie_id;
+        this.oberkategorie = oberkategorie;
+    }
 
-    public Kategorie(){}
 }
