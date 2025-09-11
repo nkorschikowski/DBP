2feecd9a0cffee346ad79c5dc57a4290f65e43ec
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 16:41:08 +0200

    Update AutorBuch.java

--- src/main/java/com/uni/AutorBuch.java
+++ src/main/java/com/uni/AutorBuch.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -12,10 +13,12 @@ import jakarta.persistence.Table;
 @Table(name = "autoren_buecher")
 public class AutorBuch{
     //To-DO: composite key
+    @Id
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "produkt_nr", referencedColumnName = "produkt_nr", nullable = true)
     private Produkt produkt;
+    @Id
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "person_id", referencedColumnName = "person_id", nullable = true)
