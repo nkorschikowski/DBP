1c639f89d3dc62cddd8bab74a62577455c72b4f6
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 16:59:03 +0200

    Update Person.java

--- src/main/java/com/uni/Person.java
+++ src/main/java/com/uni/Person.java
@@ -1,18 +1,46 @@
 package com.uni;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "personen")
-public class Person{
+public class Person {
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int person_id;
+
     private String name;
-    //TODO: FK
 
 
-    public Person(){}
+    // Getter and Setter for person_id
+    public int getPerson_id() {
+        return person_id;
+    }
+
+    public void setPerson_id(int person_id) {
+        this.person_id = person_id;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Constructor
+    public Person(int person_id, String name, Person person) {
+        this.person_id = person_id;
+        this.name = name;
+    }
+
+    // Default constructor for JPA
+    public Person() {
+    }
 }
