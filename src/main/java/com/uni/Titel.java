74f5085056d2bdbdf99c1a09c867a37dc301954f
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 17:05:37 +0200

    Update Titel.java

--- src/main/java/com/uni/Titel.java
+++ src/main/java/com/uni/Titel.java
@@ -1,19 +1,60 @@
 package com.uni;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "titel")
-public class Titel{
+public class Titel {
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private int titel_id; //TODO: serial
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int titel_id;
     private String name;
-    private String produkt_nr; 
-    //TODO: FK
 
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "produkt_nr", referencedColumnName = "produkt_nr", nullable = false)
+    private Produkt produkt;
 
-    public Titel(){}
+    public Titel() {
+        // Default constructor for JPA
+    }
+
+    public Titel(int titel_id, String name, Produkt produkt) {
+        this.titel_id = titel_id;
+        this.name = name;
+        this.produkt = produkt;
+    }
+
+    public int getTitel_id() {
+        return titel_id;
+    }
+
+    public void setTitel_id(int titel_id) {
+        this.titel_id = titel_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Produkt getProdukt() {
+        return produkt;
+    }
+
+    public void setProdukt(Produkt produkt) {
+        this.produkt = produkt;
+    }
 }
