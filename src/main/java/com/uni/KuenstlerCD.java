3551c2a3c7e52f85584344f5ee19fdaa1a08525c
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 16:53:10 +0200

    Update KuenstlerCD.java

--- src/main/java/com/uni/KuenstlerCD.java
+++ src/main/java/com/uni/KuenstlerCD.java
@@ -1,18 +1,50 @@
 package com.uni;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "kuenstler_cds")
-public class KuenstlerCD{
-    @Id //TODO: composite key
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private String produkt_nr;
-    private int person_id;
-    //TODO: FK
+public class KuenstlerCD {
 
+    @Id // TODO: composite key (should use @EmbeddedId or @IdClass for real composite key)
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "produkt_nr", referencedColumnName = "produkt_nr", nullable = false)
+    private Produkt produkt;
 
-    public KuenstlerCD(){}
+    @Id // TODO: composite key (should use @EmbeddedId or @IdClass for real composite key)
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "person_id", referencedColumnName = "person_id", nullable = false)
+    private Person person;
+
+    public KuenstlerCD() {}
+
+    public KuenstlerCD(Produkt produkt, Person person) {
+        this.produkt = produkt;
+        this.person = person;
+    }
+
+    public Produkt getProdukt() {
+        return produkt;
+    }
+
+    public void setProdukt(Produkt produkt) {
+        this.produkt = produkt;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 }
