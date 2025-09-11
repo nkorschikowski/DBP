e48115fe3247362171ed6a09b8ad82797f982fd8
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 17:05:08 +0200

    Update Rezension.java

--- src/main/java/com/uni/Rezension.java
+++ src/main/java/com/uni/Rezension.java
@@ -1,22 +1,83 @@
 package com.uni;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "rezensionen")
-public class Rezension{
-    @Id //TODO: composite key
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private int person_id;
-    private String produkt_nr;
-    //private date date; //TODO: date
+public class Rezension {
+
+    @Id // TODO: composite key
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "person_id", referencedColumnName = "person_id", nullable = false)
+    private Person person;
+
+    @Id // TODO: composite key
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "produkt_nr", referencedColumnName = "produkt_nr", nullable = false)
+    private Produkt produkt;
+
+    // private date date; //TODO: date
     private String summary;
     private int bewertung; //TODO: small int
     private String content;
-    //TODO: FK
 
+    public Person getPerson() {
+        return person;
+    }
 
-    public Rezension(){}
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Produkt getProdukt() {
+        return produkt;
+    }
+
+    public void setProdukt(Produkt produkt) {
+        this.produkt = produkt;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public int getBewertung() {
+        return bewertung;
+    }
+
+    public void setBewertung(int bewertung) {
+        this.bewertung = bewertung;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Rezension() {
+    }
+
+    public Rezension(Person person, Produkt produkt, String summary, int bewertung, String content) {
+        this.person = person;
+        this.produkt = produkt;
+        this.summary = summary;
+        this.bewertung = bewertung;
+        this.content = content;
+    }
 }
