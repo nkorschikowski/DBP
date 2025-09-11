c5dc101359c37dc6f91fc206a69f34a1bfb0d7c5
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 16:55:06 +0200

    Update Kunde.java

--- src/main/java/com/uni/Kunde.java
+++ src/main/java/com/uni/Kunde.java
@@ -1,19 +1,64 @@
 package com.uni;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "kunden")
-public class Kunde{
+public class Kunde {
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private int person_id;
-    private int adress_id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "person_id", referencedColumnName = "person_id", nullable = true)
+    private Person person;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "adress_id", referencedColumnName = "adress_id", nullable = true)
+    private Adresse adresse;
+
     private int kontonummer;
-    //TODO: FK
 
+    public Kunde() {}
 
-    public Kunde(){}
+    public Kunde(Person person, Adresse adresse, int kontonummer) {
+        this.person = person;
+        this.adresse = adresse;
+        this.kontonummer = kontonummer;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
+    public int getKontonummer() {
+        return kontonummer;
+    }
+
+    public void setKontonummer(int kontonummer) {
+        this.kontonummer = kontonummer;
+    }
 }
