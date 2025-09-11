d60ee15fdf91a4c2bfa61c0f4a95d677671033f0
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 14:35:13 +0200

    annotated hibernate, getter and setter added

--- src/main/java/com/uni/Adresse.java
+++ src/main/java/com/uni/Adresse.java
@@ -1,6 +1,8 @@
 package com.uni;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -8,14 +10,62 @@ import jakarta.persistence.Table;
 @Table(name = "adressen")
 public class Adresse{
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private int adress_id; //TODO: serial
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private int adress_id; 
     private String straße;
     private String hausnummer;
     private String zusatz;
     private String plz;
     private String stadt;
 
+    private int get_adress_id(){
+        return adress_id;
+    }
+    private void set_adress_id(int adress_id){
+        this.adress_id = adress_id;
+    }
+    private String get_straße(){
+        return straße;
+    }
+    private void set_straße(String straße){
+        this.straße = straße;
+    }
+    private String get_hausnummer(){
+        return hausnummer;
+    }
+    private void set_hausnummer(String hausnummer){
+        this.hausnummer = hausnummer;
+    }
+    private String get_zusatz(){
+        return zusatz;
+    }
+    private void set_zusatz(String zusatz){
+        this.zusatz = zusatz;
+    }
+    private String get_plz(){
+        return plz;
+    }
+    private void set_plz(String plz){
+        this.plz = plz;
+    }
+    private String get_stadt(){
+        return stadt;
+    }
+    private void set_stadt(String stadt){
+        this.stadt = stadt;
+    }
 
-    public Adresse(){}
+    public Adresse(int adress_id,
+    String straße,
+    String hausnummer,
+    String zusatz,
+    String plz,
+    String stadt){
+        this.adress_id = adress_id;
+        this.straße = straße;
+        this.hausnummer = hausnummer;
+        this.zusatz = zusatz;
+        this.plz = plz;
+        this.stadt = stadt;
+    }
 }
