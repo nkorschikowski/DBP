8831b6e56cf18ef344c8804c9cae75bedc1a147e
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 14:35:43 +0200

    Update AehnlicheProdukte.java

--- src/main/java/com/uni/AehnlicheProdukte.java
+++ src/main/java/com/uni/AehnlicheProdukte.java
@@ -1,17 +1,41 @@
 package com.uni;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "aehnliche_produkte")
 public class AehnlicheProdukte {
     @Id //TODO: composite key
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private String produkt_nr1;
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "produkt_nr", referencedColumnName = "produkt_nr", nullable = true)
+    private Produkt produkt;
+    @Id
     private String produkt_nr2;
-    //TODO: FK
 
-    public AehnlicheProdukte(){}
+    private Produkt get_produkt(){
+        return produkt;
+    }
+    private void set_produkt(Produkt produkt_nr1){
+        this.produkt = produkt;
+    }
+    private String get_produkt_nr2(){
+        return produkt_nr2;
+    }
+    private void set_produkt_nr2(String produkt_nr2){
+        this.produkt_nr2 = produkt_nr2;
+    }
+
+    public AehnlicheProdukte(Produkt produkt_nr1,
+    String produkt_nr2){
+        this.produkt = produkt;
+        this.produkt_nr2 = produkt_nr2; 
+    }
 }
