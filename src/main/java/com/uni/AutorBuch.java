c010dae3b17e690ab51f4f5bec0fecb21f049462
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-11 14:36:26 +0200

    Update AutorBuch.java

--- src/main/java/com/uni/AutorBuch.java
+++ src/main/java/com/uni/AutorBuch.java
@@ -1,19 +1,49 @@
 package com.uni;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "autoren_buecher")
 public class AutorBuch{
-    @Id //TODO: composite key
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private String produkt_nr;
-    private int person_id;
+    //To-DO: composite key
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "produkt_nr", referencedColumnName = "produkt_nr", nullable = true)
+    private Produkt produkt;
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "person_id", referencedColumnName = "person_id", nullable = true)
+    private Person person;
     
-    //TODO: FK
 
+    public Produkt getProdukt() {
+        return produkt;
+    }
 
-    public AutorBuch(){}
+    public void setProdukt(Produkt produkt) {
+        this.produkt = produkt;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public AutorBuch() {
+        // Default constructor for JPA
+    }
+
+    public AutorBuch(Produkt produkt, Person person) {
+        this.produkt = produkt;
+        this.person = person;
+    }
 }
