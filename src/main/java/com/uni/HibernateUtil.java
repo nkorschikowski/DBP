e4ad3f8b3b339ad32018204bfaa848d992fb0a1b
Nico <167690530+nkorschikowski@users.noreply.github.com>
2025-09-13 17:22:55 +0200

    läuft jetzt auch über dp.properties file

--- src/main/java/com/uni/HibernateUtil.java
+++ src/main/java/com/uni/HibernateUtil.java
@@ -15,15 +15,18 @@ public class HibernateUtil {
             // nutzt classpath statt hardcoded filepath
             props.load(HibernateUtil.class.getClassLoader().getResourceAsStream("db.properties"));
 
-            // überschreibt (nur diese) Properties aus der hibernate.cfg
             Configuration configuration = new Configuration();
+            // lädt die Configuration mit der cfg-Datei
+            configuration.configure("hibernate.cfg.xml");
+            
+            // überschreibt (nur diese) Properties aus der hibernate.cfg
             configuration.setProperty("hibernate.connection.driver_class", props.getProperty("driver"));
             configuration.setProperty("hibernate.connection.url", props.getProperty("url"));
             configuration.setProperty("hibernate.connection.username", props.getProperty("name"));
             configuration.setProperty("hibernate.connection.password", props.getProperty("password"));
-
+            
             // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure().buildSessionFactory();
+            return configuration.buildSessionFactory();
         } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
