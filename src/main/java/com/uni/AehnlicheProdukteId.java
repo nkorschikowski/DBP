53212cc273ddeefa476fdffc9e6fff8cd6a443ac
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-13 12:02:16 +0200

    Add files via upload

---
+++ src/main/java/com/uni/AehnlicheProdukteId.java
@@ -0,0 +1,41 @@
+package com.uni;
+
+import java.io.Serializable;
+
+
+public class AehnlicheProdukteId implements Serializable {
+    private String produkt_nr1;
+    private String produkt_nr2;
+
+    public AehnlicheProdukteId() {}
+
+    public AehnlicheProdukteId(String produkt_nr1, String produkt_nr2) {
+        this.produkt_nr1 = produkt_nr1;
+        this.produkt_nr2 = produkt_nr2;
+    }
+    private String get_produkt_nr1(){
+        return produkt_nr1;
+    }
+    private void set_produkt_nr1(String produkt_nr1){
+        this.produkt_nr1 = produkt_nr1;
+    }
+    private String get_produkt_nr2(){
+        return produkt_nr2;
+    }
+    private void set_produkt_nr2(String produkt_nr2){
+        this.produkt_nr2 = produkt_nr2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AehnlicheProdukteId)) return false;
+        AehnlicheProdukteId that = (AehnlicheProdukteId) o;
+        return produkt_nr1.equals(that.produkt_nr1) && produkt_nr2.equals(that.produkt_nr2);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(produkt_nr1, produkt_nr2);
+    } 
+}
---
+++ src/main/java/com/uni/AutorBuchId.java
@@ -0,0 +1,47 @@
+package com.uni;
+
+import java.io.Serializable;
+
+
+public class AutorBuchId implements Serializable {
+    private int person_id;
+    private String produkt_nr;
+
+    public AutorBuchId() {}
+
+    public AutorBuchId(int person_id, String produkt_nr) {
+        this.person_id = person_id;
+        this.produkt_nr = produkt_nr;
+    }
+
+    public int getPerson_id() {
+        return person_id;
+    }
+
+    public void setPerson_id(int person_id) {
+        this.person_id = person_id;
+    }
+
+    public String getProdukt_nr() {
+        return produkt_nr;
+    }
+
+    public void setProdukt_nr(String produkt_nr) {
+        this.produkt_nr = produkt_nr;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AutorBuchId)) return false;
+        AutorBuchId that = (AutorBuchId) o;
+        return person_id == that.person_id &&
+               java.util.Objects.equals(produkt_nr, that.produkt_nr);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(person_id, produkt_nr);
+    }
+
+}
