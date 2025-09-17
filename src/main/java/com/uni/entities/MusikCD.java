9a61b31b18b6e4611e29d4fc1f450fcb4f4b0a2e
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-17 16:45:00 +0200

    Update MusikCD.java

--- src/main/java/com/uni/entities/MusikCD.java
+++ src/main/java/com/uni/entities/MusikCD.java
@@ -5,45 +5,58 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "musikcds")
+@IdClass(MusikCDId.class)
 public class MusikCD {
     @Id
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "produkt_nr", referencedColumnName = "produkt_nr", nullable = true)
-    private Produkt produkt;
+    private Produkt produkt_id;
     private String label;
     //private date erscheinungsdatum; //TODO: date
 
-    public Produkt getProdukt() {
-        return produkt;
+    public Produkt get_Produkt_id() {
+        return produkt_id;
     }
 
-    public void setProdukt(Produkt produkt) {
-        this.produkt = produkt;
+    public void set_Produkt_id(Produkt produkt_id) {
+        this.produkt_id = produkt_id;
     }
 
-    public String getLabel() {
+    public String get_Label() {
         return label;
     }
 
-    public void setLabel(String label) {
+    public void set_Label(String label) {
         this.label = label;
     }
     //private date erscheinungsdatum(){return erscheinungsdatum;} //TODO: date
     //private void setErscheinungsdatum(date erscheinungsdatum){this.erscheinungsdatum = erscheinungsdatum;} //TODO: date
 
-    public MusikCD(Produkt produkt, String label) {
-        this.produkt = produkt;
+    public MusikCD(Produkt produkt_id, String label) {
+        this.produkt_id = produkt_id;
         this.label = label;
     }
 
     public MusikCD() {
         // Default constructor for JPA
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MusikCD)) return false;
+        MusikCD that = (MusikCD) o;
+        return produkt_id != null && produkt_id.equals(that.produkt_id);
+    }
+    @Override
+    public int hashCode() {
+        return 31 + (produkt_id != null ? produkt_id.hashCode() : 0); 
     }
 }
