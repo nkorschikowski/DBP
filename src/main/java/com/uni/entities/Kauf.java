6aed02a92767ce032b1f9bfbf18a5360631b72f8
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-17 16:37:59 +0200

    Update Kauf.java

--- src/main/java/com/uni/entities/Kauf.java
+++ src/main/java/com/uni/entities/Kauf.java
@@ -21,43 +21,43 @@ public class Kauf {
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "filiale_id", referencedColumnName = "filiale_id", nullable = true)
-    private Filiale filiale;
+    private Filiale filiale_id;
 
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "person_id", referencedColumnName = "person_id", nullable = true)
-    private Person person;
+    private Person person_id;
 
     // TODO: Date kaufdatum
 
-    public int getKauf_id() {
+    public int get_Kauf_id() {
         return kauf_id;
     }
 
-    public void setKauf_id(int kauf_id) {
+    public void set_Kauf_id(int kauf_id) {
         this.kauf_id = kauf_id;
     }
 
-    public Filiale getFiliale() {
-        return filiale;
+    public Filiale get_Filiale_id() {
+        return filiale_id;
     }
 
-    public void setFiliale(Filiale filiale) {
-        this.filiale = filiale;
+    public void set_Filiale(Filiale filiale_id) {
+        this.filiale_id = filiale_id;
     }
 
-    public Person getPerson() {
-        return person;
+    public Person get_Person_id() {
+        return person_id;
     }
 
-    public void setPerson(Person person) {
-        this.person = person;
+    public void set_Person_id(Person person_id) {
+        this.person_id = person_id;
     }
 
-    public Kauf(int kauf_id, Filiale filiale, Person person) {
+    public Kauf(int kauf_id, Filiale filiale_id, Person person_id) {
         this.kauf_id = kauf_id;
-        this.filiale = filiale;
-        this.person = person;
+        this.filiale_id = filiale_id;
+        this.person_id = person_id;
     }
 
     public Kauf() {
