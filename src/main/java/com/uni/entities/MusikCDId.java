b2c7fd92ba2f66f0d7c7f4a249d4df634126181d
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-17 16:45:36 +0200

    Update MusikCDId.java

--- src/main/java/com/uni/entities/MusikCDId.java
+++ src/main/java/com/uni/entities/MusikCDId.java
@@ -1,5 +1,35 @@
 package com.uni.entities;
 
-public class MusikCDId {
-    
+import java.io.Serializable;
+import java.util.Objects;
+
+public class MusikCDId implements Serializable {
+    private String produkt_nr;  
+ 
+    public MusikCDId() {}
+
+    public MusikCDId(String produkt_nr) {
+        this.produkt_nr = produkt_nr;
+    }
+
+    public String get_produkt_nr() {
+        return produkt_nr;
+    }
+    public void set_produkt_nr(String produkt_nr) {
+        this.produkt_nr = produkt_nr;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MusikCDId)) return false;
+        MusikCDId that = (MusikCDId) o;
+        return Objects.equals(produkt_nr, that.produkt_nr);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(produkt_nr);
+    }
 }
