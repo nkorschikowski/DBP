39868cc5064b6e147bfd30fbc5099cb0cb4024cc
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-17 16:49:56 +0200

    Update ProduktKategorieId.java

--- src/main/java/com/uni/entities/ProduktKategorieId.java
+++ src/main/java/com/uni/entities/ProduktKategorieId.java
@@ -2,29 +2,29 @@ package com.uni.entities;
 
 public class ProduktKategorieId {
     private String produkt_nr;
-    private String kategorie;
+    private int kategorie_id;
 
     public ProduktKategorieId() {}
 
-    public ProduktKategorieId(String produkt_nr, String kategorie) {
+    public ProduktKategorieId(String produkt_nr, int kategorie_id) {
         this.produkt_nr = produkt_nr;
-        this.kategorie = kategorie;
+        this.kategorie_id = kategorie_id;
     }
 
-    public String getProdukt_nr() {
+    public String get_Produkt_nr() {
         return produkt_nr;
     }
 
-    public void setProdukt_nr(String produkt_nr) {
+    public void set_Produkt_nr(String produkt_nr) {
         this.produkt_nr = produkt_nr;
     }
 
-    public String getKategorie() {
-        return kategorie;
+    public int get_Kategorie_id() {
+        return kategorie_id;
     }
 
-    public void setKategorie(String kategorie) {
-        this.kategorie = kategorie;
+    public void set_Kategorie_id(int kategorie_id) {
+        this.kategorie_id = kategorie_id;
     }
 
     @Override
@@ -32,11 +32,11 @@ public class ProduktKategorieId {
         if (this == o) return true;
         if (!(o instanceof ProduktKategorieId)) return false;
         ProduktKategorieId that = (ProduktKategorieId) o;
-        return produkt_nr.equals(that.produkt_nr) && kategorie.equals(that.kategorie);
+        return produkt_nr.equals(that.produkt_nr) && kategorie_id == that.kategorie_id;
     }
 
     @Override
     public int hashCode() {
-        return java.util.Objects.hash(produkt_nr, kategorie);
+        return java.util.Objects.hash(produkt_nr, kategorie_id);
     }
 }
