1c67df330919dbb50c36fc26a6e4bdb69c747819
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-17 16:33:18 +0200

    Update DVD.java

--- src/main/java/com/uni/entities/DVD.java
+++ src/main/java/com/uni/entities/DVD.java
@@ -5,28 +5,30 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "dvds")
+@IdClass(DVDId.class)
 public class DVD{
     
     @Id
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "produkt_nr", referencedColumnName = "produkt_nr", nullable = true)
-    private Produkt produkt;
+    private Produkt produkt_nr;
     private String format;
     //private time laufzeit; //TODO: time
     private int region_code; //TODO: small int?
 
-    public Produkt get_produkt() {
-        return produkt;
+    public Produkt get_produkt_nr() {
+        return produkt_nr;
     }
-    public void set_produkt_nr(Produkt produkt) {
-        this.produkt = produkt;
+    public void set_produkt_nr(Produkt produkt_nr) {
+        this.produkt_nr = produkt_nr;
     }
     public String get_format() {
         return format;
@@ -41,12 +43,26 @@ public class DVD{
         this.region_code = region_code;
     }
 
-    public DVD(Produkt produkt,
+    public DVD(Produkt produkt_nr,
     String format,
     int region_code) {
-        this.produkt = produkt;
+        this.produkt_nr = produkt_nr;
         this.format = format;
         this.region_code = region_code;
+    }
+    public DVD() {
+        // Default constructor for JPA
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DVD)) return false;
+        DVD that = (DVD) o;
+        return produkt_nr != null && produkt_nr.equals(that.produkt_nr);
+    }
+    @Override
+    public int hashCode() {
+        return 31 + (produkt_nr != null ? produkt_nr.hashCode() : 0); 
     }
 
 }
