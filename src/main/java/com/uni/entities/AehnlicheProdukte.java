59f1051b3c068eb284f1ab98e4aa7756231f4f73
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-17 16:14:05 +0200

    Update AehnlicheProdukte.java

--- src/main/java/com/uni/entities/AehnlicheProdukte.java
+++ src/main/java/com/uni/entities/AehnlicheProdukte.java
@@ -5,26 +5,28 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "aehnliche_produkte")
+@IdClass(AehnlicheProdukteId.class)
 public class AehnlicheProdukte {
-    @Id //TODO: composite key
+    @Id //composite key
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "produkt_nr", referencedColumnName = "produkt_nr", nullable = true)
-    private Produkt produkt;
+    @JoinColumn(name = "produkt_nr1", referencedColumnName = "produkt_nr", nullable = true)
+    private Produkt produkt_nr1;
     @Id
     private String produkt_nr2;
 
-    public Produkt get_produkt(){
-        return produkt;
+    public Produkt get_produkt_nr1(){
+        return produkt_nr1;
     }
-    public void set_produkt(Produkt produkt_nr1){
-        this.produkt = produkt;
+    public void set_produkt_nr1(Produkt produkt_nr1){
+        this.produkt_nr1 = produkt_nr1;
     }
     public String get_produkt_nr2(){
         return produkt_nr2;
@@ -35,7 +37,25 @@ public class AehnlicheProdukte {
 
     public AehnlicheProdukte(Produkt produkt_nr1,
     String produkt_nr2){
-        this.produkt = produkt;
+        this.produkt_nr1 = produkt_nr1;
         this.produkt_nr2 = produkt_nr2; 
+    }
+    public AehnlicheProdukte(){
+        // Default constructor for JPA
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AehnlicheProdukte)) return false;
+        AehnlicheProdukte that = (AehnlicheProdukte) o;
+        return produkt_nr1 != null && produkt_nr1.equals(that.produkt_nr1) &&
+               produkt_nr2 != null && produkt_nr2.equals(that.produkt_nr2);
+    }
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (produkt_nr1 != null ? produkt_nr1.hashCode() : 0);
+        result = 31 * result + (produkt_nr2 != null ? produkt_nr2.hashCode() : 0);
+        return result;
     }
 }
