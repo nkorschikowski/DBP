e53bd6dbecb0eb128a8d5ed1d96b4d159a637b31
merlinEgress <merlin.hofmann@mailbox.org>
2025-09-17 16:41:16 +0200

    Update KaufProduktId.java

--- src/main/java/com/uni/entities/KaufProduktId.java
+++ src/main/java/com/uni/entities/KaufProduktId.java
@@ -1,30 +1,30 @@
 package com.uni.entities;
 
 public class KaufProduktId {
-    private int kauf;
-    private int angebot;
+    private int kauf_id;
+    private int angebot_id;
 
     public KaufProduktId() {}
 
-    public KaufProduktId(int kauf, int angebot) {
-        this.kauf = kauf;
-        this.angebot = angebot;
+    public KaufProduktId(int kauf_id, int angebot_id) {
+        this.kauf_id = kauf_id;
+        this.angebot_id = angebot_id;
     }
 
-    public int getKauf() {
-        return kauf;
+    public int get_Kauf_id() {
+        return kauf_id;
     }
 
-    public void setKauf(int kauf) {
-        this.kauf = kauf;
+    public void set_Kauf_id(int kauf_id) {
+        this.kauf_id = kauf_id;
     }
 
-    public int getAngebot() {
-        return angebot;
+    public int get_Angebot_id() {
+        return angebot_id;
     }
 
-    public void setAngebot(int angebot) {
-        this.angebot = angebot;
+    public void set_Angebot_id(int angebot_id) {
+        this.angebot_id = angebot_id;
     }
 
     @Override
@@ -32,11 +32,11 @@ public class KaufProduktId {
         if (this == o) return true;
         if (!(o instanceof KaufProduktId)) return false;
         KaufProduktId that = (KaufProduktId) o;
-        return kauf == that.kauf && angebot == that.angebot;
+        return kauf_id == that.kauf_id && angebot_id == that.angebot_id;
     }
 
     @Override
     public int hashCode() {
-        return java.util.Objects.hash(kauf, angebot);
+        return java.util.Objects.hash(kauf_id, angebot_id);
     }
 }
